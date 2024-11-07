9d331f32d7a6acefa20dda77e816d12d10aa35e2
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2024-11-07 11:34:21 +0100

    arrange app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './screens/home/index'
-import { routes } from './routes';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./screens/home/index";
+import { routes } from "./routes";
 
 function App() {
-
-  const appRoutes = [
-    { path: routes.page .home, element: <HomePage /> },
-
-  
-
-
-  ];
+  const appRoutes = [{ path: routes.page.home, element: <HomePage /> }];
   return (
     <Routes>
       <Route>
-        {appRoutes.map((item,index) => (
+        {appRoutes.map((item, index) => (
           <Route key={index} path={item.path} element={item.element} />
         ))}
       </Route>
-      
-
-     
     </Routes>
   );
 }
 
-export default App
+export default App;
