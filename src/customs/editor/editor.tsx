eb4e3fa90f35eff31d0c109dceca23525cb02ad9
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2024-12-24 08:04:25 +0100

    build error fix

--- src/customs/editor/editor.tsx
+++ src/customs/editor/editor.tsx
@@ -1,5 +1,6 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"; // Ensure this is the correct import
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { ClassicEditor } from "ckeditor5";
 import { ErrorMessage } from "formik";
 
 interface EditorProps {
@@ -11,7 +12,9 @@ interface EditorProps {
 
 const Editor = ({ label, name, onChange, initialData }: EditorProps) => {
   return (
-    <div className="editor-container"> {/* Apply a custom container class */}
+    <div className="editor-container">
+      {" "}
+      {/* Apply a custom container class */}
       <label>{label}</label>
       <CKEditor
         editor={ClassicEditor}
@@ -31,12 +34,13 @@ const Editor = ({ label, name, onChange, initialData }: EditorProps) => {
           ],
         }}
         onChange={(event, editor) => {
-          console.log(event)
+          console.log(event);
           if (onChange && name) {
             onChange(name, editor); // Pass editor instance for use
           }
         }}
       />
+      
       {name && (
         <ErrorMessage name={name}>
           {(msg) => <span className="error">{msg}</span>}
